2797e902b3ec2963f00978b89a1e40435e9c7762
Kondri Indumathi <kondriindhu1@gmail.com>
2026-02-21 18:51:22 +0530

    Add files via upload

---
+++ datatypes.java
@@ -0,0 +1,22 @@
+class data
+{
+public static void main(String args[])
+{
+short s=5000;
+byte b=100;
+int i=1;
+float f=3.6f;
+double d=30.29;
+String s1="chandu";
+char c='A';
+boolean e=true;
+System.out.println("short:"+s);
+System.out.println("byte:"+b);
+System.out.println("integer:"+i);
+System.out.println("float:"+f);
+System.out.println("double:"+d);
+System.out.println("string:"+s1);
+System.out.println("char:"+c);
+System.out.println("boolean:"+e);
+}
+}
