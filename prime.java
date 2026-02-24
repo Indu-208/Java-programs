c28edaa5a238fff2a0605ea0faac2006556b3b37
Kondri Indumathi <kondriindhu1@gmail.com>
2026-02-24 20:01:49 +0530

    Add files via upload

---
+++ prime.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+class prime {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        System.out.println("Prime numbers up to " + n + ":");
+        for(int i = 2; i <= n; i++) {
+            int count = 0;
+            for(int j = 1; j <= i; j++) {
+                if(i % j == 0)
+                    count++;
+            }
+            if(count == 2)
+                System.out.println(i);
+        }
+    }
+}
