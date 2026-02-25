33f71a498b3c85b6c439e0b15d380ec7608f7867
Kondri Indumathi <kondriindhu1@gmail.com>
2026-02-25 21:00:05 +0530

    Add files via upload

---
+++ ArithmeticOperations.java
@@ -0,0 +1,13 @@
+class ArithmeticOperations
+  {
+    public static void main()
+    {
+      int a=10;
+      double b=5.5;
+      System.out.println("Addition:"+(a+b));
+      System.out.println("Subtraction:"+(a-b));
+      System.out.println("Multiplication:"+(a*b));
+      System.out.println("Division:"+(a/b));
+      System.out.println("Module:"+(a%b));
+    }
+  }
