e7b82b4ebf4b0e2b1cf6c0d6463a77f7eff982ee
Kondri Indumathi <kondriindhu1@gmail.com>
2026-03-03 18:52:40 +0530

    Add files via upload

---
+++ DefaultCon.java
@@ -0,0 +1,16 @@
+class DefaultCon
+{
+int age;
+String name;
+DefaultCon()
+{
+age=20;
+name="Indhu";
+}
+public static void main(String args[])
+{
+DefaultCon obj = new DefaultCon();
+System.out.println("name:" + obj.name + "  age:" + obj.age);
+}
+}
+
