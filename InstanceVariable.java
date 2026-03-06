e99a3f635d46b2490886565de8e1065bc8602ebe
Kondri Indumathi <kondriindhu1@gmail.com>
2026-03-06 20:16:30 +0530

    Add files via upload

---
+++ InstanceVariable.java
@@ -0,0 +1,10 @@
+class InstanceVariable
+{
+	int a=8;
+	public static void main(String args[])
+	{
+		InstanceVariable obj=new InstanceVariable();
+		System.out.println("Instance Variable Value:"+obj.a);
+		
+	}
+}
