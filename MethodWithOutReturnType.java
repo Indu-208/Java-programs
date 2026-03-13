88f05f62b7449201a1037df726183bfd2c41a45a
Kondri Indumathi <kondriindhu1@gmail.com>
2026-03-13 22:02:49 +0530

    Add files via upload

---
+++ MethodWithOutReturnType.java
@@ -0,0 +1,12 @@
+class MethodWithOutReturnType
+{
+	void add(int a,int b)
+	{
+		System.out.println(a+b);
+	}
+	public static void main(String args[])
+	{
+		 MethodWithOutReturnType m=new  MethodWithOutReturnType();
+		 m.add(5,2);
+	}
+}
