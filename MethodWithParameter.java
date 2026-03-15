2c8ffd9b0d83f3708be2936ff29d80637070a405
Kondri Indumathi <kondriindhu1@gmail.com>
2026-03-15 19:34:24 +0530

    Add files via upload

---
+++ MethodWithParameter.java
@@ -0,0 +1,12 @@
+class MethodWithParameter
+{
+	void show(String name)
+	{
+		System.out.println(" " +name);
+	}
+	public static void main(String args[])
+	{
+		 MethodWithParameter m=new MethodWithParameter();
+		 m.show("Indhu");
+	}
+}
