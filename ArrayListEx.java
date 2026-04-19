506a0f0a5401529e029c63888604ee9aa158b4e5
Kondri Indumathi <kondriindhu1@gmail.com>
2026-04-19 08:15:03 +0530

    Add files via upload

---
+++ ArrayListEx.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class ArrayListEx {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+
+        list.add("Java");
+        list.add("Python");
+        list.add("C++");
+
+        System.out.println(list);
+
+        list.remove("Python");
+        System.out.println(list);
+    }
+}
