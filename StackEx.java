2357dc2b2e320712ada47ee112648fba408b201e
Kondri Indumathi <kondriindhu1@gmail.com>
2026-04-27 20:06:11 +0530

    Add files via upload

---
+++ StackEx.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class StackEx {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+
+        System.out.println("Top element: " + stack.peek());
+
+        while(!stack.isEmpty()) {
+            System.out.println(stack.pop());
+        }
+    }
+}
