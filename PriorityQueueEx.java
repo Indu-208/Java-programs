1438716e70f2947f3a704109b45aaa1e674b44c3
Kondri Indumathi <kondriindhu1@gmail.com>
2026-04-29 20:39:51 +0530

    Add files via upload

---
+++ PriorityQueueEx.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class PriorityQueueEx {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        pq.add(30);
+        pq.add(10);
+        pq.add(20);
+
+        while(!pq.isEmpty()) {
+            System.out.println(pq.poll()); // removes smallest first
+        }
+    }
+}
