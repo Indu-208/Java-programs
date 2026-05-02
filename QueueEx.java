f5217b3f5edda4609c39ae4cff43b4b7f7f2f28c
Kondri Indumathi <kondriindhu1@gmail.com>
2026-05-02 18:48:33 +0530

    Add files via upload

---
+++ QueueEx.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class QueueEx {
+    public static void main(String[] args) {
+        Queue<Integer> q = new LinkedList<>();
+
+        q.add(10);
+        q.add(20);
+        q.add(30);
+
+        System.out.println("Queue: " + q);
+
+        q.remove();
+        System.out.println("After remove: " + q);
+    }
+}
