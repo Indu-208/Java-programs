57b59f2dd44a174c2faab6f829770ba89d3e2668
Kondri Indumathi <kondriindhu1@gmail.com>
2026-05-09 20:09:06 +0530

    Add files via upload

---
+++ VectorEx.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class VectorEx {
+    public static void main(String[] args) {
+        Vector<String> v = new Vector<>();
+
+        v.add("Java");
+        v.add("C++");
+        v.add("Python");
+
+        System.out.println(v);
+    }
+}
