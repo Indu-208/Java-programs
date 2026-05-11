3d0b5039d9e736b2cd1c813fc9352ceedb0cc475
Kondri Indumathi <kondriindhu1@gmail.com>
2026-05-11 07:07:59 +0530

    Add files via upload

---
+++ StackPalindrome.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class StackPalindrome {
+    public static void main(String[] args) {
+        String str = "madam";
+        Stack<Character> stack = new Stack<>();
+
+        for(char c : str.toCharArray()) {
+            stack.push(c);
+        }
+
+        String rev = "";
+
+        while(!stack.isEmpty()) {
+            rev += stack.pop();
+        }
+
+        if(str.equals(rev)) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
