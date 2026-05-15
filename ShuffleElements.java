2ff0d0ce44d33b24d8dff9df3d5e790ee2244039
Kondri Indumathi <kondriindhu1@gmail.com>
2026-05-15 17:54:38 +0530

    Add files via upload

---
+++ ShuffleElements.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class ShuffleElements {
+    public static void main(String[] args) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+
+        Collections.shuffle(list);
+
+        System.out.println(list);
+    }
+}
