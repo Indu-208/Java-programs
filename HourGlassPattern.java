bbc5a8e1202181fe086405410abb90e3af6ca14e
Kondri Indumathi <kondriindhu1@gmail.com>
2026-08-07 21:52:46 +0530

    Add files via upload

---
+++ HourGlassPattern.java
@@ -0,0 +1,25 @@
+public class HourGlassPattern {
+    public static void main(String[] args) {
+        int n = 5;
+
+        for (int i = 0; i < n; i++) {
+            for (int s = 0; s < i; s++)
+                System.out.print(" ");
+
+            for (int j = 0; j < 2 * (n - i) - 1; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int s = 0; s < n - i; s++)
+                System.out.print(" ");
+
+            for (int j = 0; j < 2 * i - 1; j++)
+                System.out.print("*");
+
+            System.out.println();
+        }
+    }
+}
