2709ed30ce1992de199527404f1bb3a0661bc371
Kondri Indumathi <kondriindhu1@gmail.com>
2026-08-15 21:12:00 +0530

    Add files via upload

---
+++ CommonElements.java
@@ -0,0 +1,14 @@
+public class CommonElements {
+    public static void main(String[] args) {
+        int[] a = {1, 2, 3, 4, 5};
+        int[] b = {3, 4, 5, 6, 7};
+        for (int x : a) {
+            for (int y : b) {
+                if (x == y) {
+                    System.out.print(x + " ");
+                    break;
+                }
+            }
+        }
+    }
+}
